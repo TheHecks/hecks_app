5849b9a1df801a344e6a09a78d6bcda4d27a1f74
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-08-20 13:16:22 -0700

    remove cruft

--- drivers/sql.rb
+++ drivers/sql.rb
@@ -7,5 +7,5 @@ require_relative 'sql/matches/match/repository'
 require_relative 'sql/teams/team/repository'
 require_relative 'sql/migrate.rb'
 
-ActiveRecord::Base.establish_connection adapter: "sqlite3", database: "soccer_season"
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: "soccer_season_db"
 CreateDatabase.new.up
