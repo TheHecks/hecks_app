77b1ccd6a51e89814c8bdd2489e87e8ac353d628
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-08-20 13:59:02 -0700

    move stuff into a domain folder

--- drivers/sql.rb
+++ drivers/sql.rb
@@ -1,10 +1,10 @@
 require "active_record"
 require_relative 'sql/matches_ar'
 require_relative 'sql/teams_ar'
-require_relative 'sql/matches/match'
-require_relative 'sql/teams/team'
-require_relative 'sql/matches/match/repository'
-require_relative 'sql/teams/team/repository'
+require_relative 'sql/domain/matches/match'
+require_relative 'sql/domain/teams/team'
+require_relative 'sql/domain/matches/match/repository'
+require_relative 'sql/domain/teams/team/repository'
 require_relative 'sql/migrate.rb'
 
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: "drivers/sql/database/soccer_season"
