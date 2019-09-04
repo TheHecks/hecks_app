60a206ead62e27d2394b1c7a6364d493a1311550
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-09-04 16:00:00 -0700

    put constant behind namespace

--- lib/application_port/domain_schema.rb
+++ lib/application_port/domain_schema.rb
@@ -1,4 +1,4 @@
-HECKS_FILE_NAME = 'Domainfile'.freeze
-gem_dir = Gem::Specification.find_by_name('soccer_season').gem_dir
-
-DOMAIN_SCHEMA = instance_eval(File.open("#{gem_dir}/Domainfile").read)
+module ApplicationPort
+  gem_dir = Gem::Specification.find_by_name('soccer_season').gem_dir
+  DOMAIN_SCHEMA = instance_eval(File.open("#{gem_dir}/Domainfile").read)
+end
