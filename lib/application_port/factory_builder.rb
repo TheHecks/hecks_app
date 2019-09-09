68549241e2ec8a917e44539d76d15219474ffc69
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-09-09 15:53:33 -0700

    dont require args

--- lib/application_port/factory_builder.rb
+++ lib/application_port/factory_builder.rb
@@ -6,7 +6,7 @@ module HecksApp
         return unless result.constants.include?(:Factories)
 
         result::Factories.constants.each do |factory_name|
-          app_object.define_singleton_method factory_name.to_s.underscore.downcase do |args|
+          app_object.define_singleton_method factory_name.to_s.underscore.downcase do |args={}|
             domain_aggregate.const_get(domain_object)::Factories.const_get(factory_name).factory(args)
           end
         end
