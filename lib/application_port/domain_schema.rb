1a64eddde0cd4f77b3b534a5c48e1052b2ef18ef
Chris Young <beesucker@gmail.com>
2019-10-06 20:57:39 -0700

    call for parser

--- lib/application_port/domain_schema.rb
+++ lib/application_port/domain_schema.rb
@@ -2,6 +2,5 @@ module HecksApp
   gem_dir = Gem::Specification.find_by_name(ApplicationPort.domain.to_s.underscore).gem_dir
   DOMAIN_SCHEMA =
     instance_eval(File.open("#{gem_dir}/Domainfile").read)
-    .instance_variable_get(:@parser)
     .domain.freeze
 end
